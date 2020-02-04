69dc89b7fbff16d4c559cc886638e68a6b6b590b
pwang1997 <puck19970418@gmail.com>
2020-02-03 16:29:07 -0800

    cleanup

--- application/views/questions/create.php
+++ application/views/questions/create.php
@@ -4,23 +4,6 @@
     <?php redirect('users/login'); ?>
 <?php endif; ?>
 <h2><?php echo $title; ?></h2>
-<div id="toast">
-    
-</div>
-
-<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-  <div class="toast-header">
-    <img src="..." class="rounded mr-2" alt="...">
-    <strong class="mr-auto">Bootstrap</strong>
-    <small>11 mins ago</small>
-    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
-  <div class="toast-body">
-    Hello, world! This is a toast message.
-  </div>
-</div>
 
 <div class="form_question">
     <?php echo form_open('questions/create'); ?>
