746640eb28748dd532a1df9d7a1fc88c97680e28
PuckWang123 <37675715+PuckWang123@users.noreply.github.com>
2020-03-01 20:29:19 -0800

    updated styling on users/student/ view

--- application/views/users/student.php
+++ application/views/users/student.php
@@ -1,40 +1,38 @@
 <?php if (strcmp($this->session->role, 'teacher') == 0) : ?>
   <?php redirect('home'); ?>
-  <?php elseif(empty($this->session->username)): ?>
-    <?php redirect('users/login');?>
+<?php elseif (empty($this->session->username)) : ?>
+  <?php redirect('users/login'); ?>
 <?php endif; ?>
+<h3 class="border-bottom pb-2 pt-2 mt-2">Dashboard </h3>
 
-<h2>Welcome back <?php echo $this->session->username . '!'; ?></h2>
-<?php //print_r($course_list); ?>
+<br>
 <?php
 for ($i = 0; $i < sizeof($course_list); $i++) {
-  if ($i % 3 == 0) {
-?>
-    <div class="row">
-      <?php addedCard($course_list[$i]['course_name'], $course_list[$i]['course_id'], $course_list[$i]['classroom_id']); ?>
-      <div class="col-md-1"></div>
-    <?php } elseif ($i % 3 == 2) {
-    ?>
-      <?php addedCard($course_list[$i]['course_name'], $course_list[$i]['id'], $course_list[$i]['classroom_id']); ?>
-    </div>
-  <?php } else { ?>
-    <?php addedCard($course_list[$i]['course_name'], $course_list[$i]['id'], $course_list[$i]['classroom_id']); ?>
-    <div class="col-md-1"></div>
-<?php }
-}
-?>
+  if ($i % 3 == 0) :
+    echo '<div class="card-group">';
+    addedCard($course_list[$i]['course_name'], $course_list[$i]['course_id'], $course_list[$i]['classroom_id']);
+  elseif ($i % 3 == 2) :
+    addedCard($course_list[$i]['course_name'], $course_list[$i]['course_id'], $course_list[$i]['classroom_id']);
+    echo '</div>';
+  else :
+    addedCard($course_list[$i]['course_name'], $course_list[$i]['course_id'], $course_list[$i]['classroom_id']);
+  endif;
+} ?>
+
 <?php if (sizeof($course_list) % 3 != 0) : ?>
   </div>
 <?php endif; ?>
-
 <?php
 function addedCard($course_name, $course_id, $classroom_id)
 {
-  echo ' <div class="card text-white bg-primary mb-3 col-md-3">
-        <div class="card-header">' . $course_name . '</div>
-        <div class="card-body">
-          <h4 class="card-title"><a href="../courses/student/' . $course_id . '/'. $classroom_id.'" class="text-secondary">Section' . $course_id . '</a></h4>
-        </div>
-      </div>';
+  // <img src="..." class="card-img-top" alt="...">
+  echo '
+  <div class="card card-student-course col-md-3 ml-4 mr-4 mb-4">
+    <div class="card-body">
+    <a href="../courses/student/' . $course_id . '/' . $classroom_id . '">
+      <h6 class="card-text text-center pt-5">' . $course_name . '<br>Section' . $course_id . '</h6>
+      </a>
+    </div>
+  </div>';
 }
 ?>
