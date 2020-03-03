4db8b7a623d2468b1b8be57aeb1ce59a153fc376
pwang1997 <puck19970418@gmail.com>
2020-03-03 10:29:14 -0800

    updated UI for questions/student

--- application/views/questions/student.php
+++ application/views/questions/student.php
@@ -22,49 +22,23 @@
 <div class="question_on invisible">
     <!-- content + buttons  -->
     <div class="row">
-        <div class="col-8">
-            <br>
-            <div class="form-group row" style="position:relative;">
-                <div class="col-sm-8" id="scrolling-container" style="height:425px; min-width:100%; min-height:100%">
-                    <div id="editor" style="min-height:100%; height:auto;"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-4">
-            <br>
-            <div class="d-flex flex-column">
-                <div class="p-2" id="status">Status:</div>
-                <div class="p-2" id="targeted_time">Targeted Time: </div>
-                <div class="p-2">
-                    <p id="duration">
-                    </p>
-                    <div class="progress">
-                    </div>
-                </div>
-                <div class="p-2">
-                    <button type="button" class="btn btn-primary submit">Submit</button>
-                </div>
+        <div class="p-2 col-sm-3" id="status">Status:</div>
+        <div class="p-2 col-sm-3" id="targeted_time">Targeted Time: </div>
+    </div>
+    <div class="row">
+        <div class="p-2 col-sm-12">
+            <p id="duration">
+            </p>
+            <div class="progress">
             </div>
         </div>
     </div>
+    <h6 class="ml-2" id="editor"></h6>
     <!-- answer/choices -->
-    <div>
-        <?php $choices = (isset($this->session->choices)) ? json_decode($this->session->choices) : [];
-        $i = 1;
-        foreach ($choices as $choice) : ?>
-            <div class="form-group row choice_row">
-                <label for="choice<?= $i; ?>" class="col-sm-2 col-form-label">:Choice <?= $i; ?></label>
-                <div class="col-sm-6">
-                    <input type="text" disabled class="form-control choice_row" name="choice_row" placeholder="<?= $choice; ?>">
-                </div>
-                <div class="form-check col-sm-1">
-                    <input class="form-check-input answers" type="checkbox" name="answers" value="<?= $choice; ?>">
-                </div>
-            </div>
-            <?php $i++; ?>
-        <?php
-        endforeach;
-        ?>
-    </div>
     <div class="options"></div>
+    <div class="row">
+        <div class="col-sm-12 col-md-6">
+            <button style="width:100%" type="button" class="btn btn-primary submit">Submit</button>
+        </div>
+    </div>
 </div>
