72b7a701de92f0fd20091e87cf7e0b2c7babe364
pwang1997 <puck19970418@gmail.com>
2020-04-02 18:24:22 -0700

    bug fix

--- js/courses/teacher.js
+++ js/courses/teacher.js
@@ -73,8 +73,7 @@ $(document).ready(() => {
             },
             success: function (response) {
                 if (response.success) {
-                    quiz_index = response.quiz_index;
-                    location.replace(`${root_url}/questions/create/${quiz_index}`);
+                    location.replace(`${root_url}/questions/create/${response.quiz_index}`);
                 } else {
                     alert("failed ")
                 }
