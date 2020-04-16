2e183d614a33a01bfd46738d78e8de119beaf330
pwang1997 <puck19970418@gmail.com>
2020-04-16 05:59:49 -0700

    fixing styling

--- application/views/courses/teacher.php
+++ application/views/courses/teacher.php
@@ -115,20 +115,12 @@
 
 				<button style='width:100%' id="add_quiz" class="btn btn-primary btn-lg">
 					New Quiz
-					<svg class="bi bi-plus" font-size="1.25em" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd" />
-						<path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd" />
-					</svg>
 				</button>
 			</div>
 
 			<div class="col-md-3 col-lg-3 py-2 ml-2">
-				<button style='width:100%' id="export_class_history" class="btn btn-primary btn-lg" name="<?php echo $classroom_id; ?>">Export
-					<svg class="bi bi-download" font-size="1.25em" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd" d="M.5 8a.5.5 0 01.5.5V12a1 1 0 001 1h12a1 1 0 001-1V8.5a.5.5 0 011 0V12a2 2 0 01-2 2H2a2 2 0 01-2-2V8.5A.5.5 0 01.5 8z" clip-rule="evenodd" />
-						<path fill-rule="evenodd" d="M5 7.5a.5.5 0 01.707 0L8 9.793 10.293 7.5a.5.5 0 11.707.707l-2.646 2.647a.5.5 0 01-.708 0L5 8.207A.5.5 0 015 7.5z" clip-rule="evenodd" />
-						<path fill-rule="evenodd" d="M8 1a.5.5 0 01.5.5v8a.5.5 0 01-1 0v-8A.5.5 0 018 1z" clip-rule="evenodd" />
-					</svg>
+				<button style='width:100%' id="export_class_history" class="btn btn-primary btn-lg" name="<?php echo $classroom_id; ?>">
+					Export
 				</button>
 			</div>
 		</div>
